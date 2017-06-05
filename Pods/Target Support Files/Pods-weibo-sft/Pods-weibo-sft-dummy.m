20e308134061ce4083998089d2977e6f0c5d6eb9
xiaobai1315 <jermy@JermydeMacBook-Pro.local>
2017-06-05 17:57:03 +0800

    获取授权码

---
+++ Pods/Target Support Files/Pods-weibo-sft/Pods-weibo-sft-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weibo_sft : NSObject
+@end
+@implementation PodsDummy_Pods_weibo_sft
+@end
--- weibo-sft/HJAuthViewController.swift
+++ weibo-sft/HJAuthViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Alamofire
 
-class HJAuthViewController: UIViewController, UIWebViewDelegate {
+class HJAuthViewController: UIViewController {
 
     var webView : UIWebView? = nil
     
@@ -30,32 +31,65 @@ class HJAuthViewController: UIViewController, UIWebViewDelegate {
         webView.loadRequest(URLRequest(url: url as URL))
         
     }
+}
 
+extension HJAuthViewController : UIWebViewDelegate{
+    
     //#MARK:- webView代理
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         
-        let requestUrl : String = (request.url?.absoluteString)!
+        guard (request.url != nil) else {
+            return true
+        }
         
-        //是否是回调页面
-        if !requestUrl.hasPrefix("https://api.weibo.com"){
+        /*
+         1、请求登录页面
+         "https://api.weibo.com/oauth2/authorize?client_id=402791712&redirect_uri=https://api.weibo.com/oauth2/default.html" 
+         2、登录成功会访问 "https://api.weibo.com/oauth2/default.html?code=0ccfbe20fc4c2acbd9bf0d096187d5d5" ”code=“包含的是授权码
+         3、
+         */
+        
+        
+        let requestUrl : String = (request.url?.absoluteString)!
+
+        //不是授权回调页面，返回true
+        if !requestUrl.hasPrefix("https://api.weibo.com/oauth2/default.html"){
             
+            return true
+        }
+
+        //判断返回的授权码是否包含‘code=’字段
+        let key = "code="
+        
+        print(requestUrl)
+        
+        let range = (requestUrl as NSString).range(of: key)
+        if range.length == 0{
+            print("授权失败")
             return false
         }
         
-        let key = "code="
+        //截取授权码
+        let code = (requestUrl as NSString).substring(from: (range.location + range.length))
         
-//        let range = (requestUrl as NSString).range(of: key)
+        if(code as NSString).length == 0{
+            return false;
+        }
         
-        let code = requestUrl.substring(from: key.endIndex)
-        
-//        if range.length == 0{
-//            return true
-//        }
-        
+        //获取accessToken
         
         
-        return false
+        return true
     }
     
-
+    //获取AccessToken
+    private func getAccessToken()->Void{
+        
+        let url : URL = URL(string: "")!
+        let parameters : [String : Any] = ["foo" : "123"]
+        
+        
+        Alamofire.request(url!, method: HTTPMethod.post, parameters: parameters, encoding: ParameterEncoding, headers: <#T##HTTPHeaders?#>)
+        
+    }
 }
